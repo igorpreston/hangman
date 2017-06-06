971ed13b5fb7fa61fbdc0a6af34f0a4288b4a70d
Igor Preston <igorpreston@gmail.com>
2017-06-06 22:06:16 +0300

    add  folk head retina versions with srcset

--- src/components/HangedMan/Folk/FolkHead/index.jsx
+++ src/components/HangedMan/Folk/FolkHead/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import classNames from 'classnames/bind';
-import head from './head.png';
+import head from './head/head.png';
+import head2x from './head/head@2x.png';
+import head3x from './head/head@3x.png';
 import styles from './styles';
 
 const FolkHead = () => (
   <img 
     src={head}
+    srcset={`${head} 1x, ${head2x} 2x, ${head3x} 3x`}
     className={classNames.bind(styles)({
       head: true,
     })}
